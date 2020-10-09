11a4c67472ddea802849ebc03798912ecd3d6919
theolamide <ajadidan@gmail.com>
2020-10-09 00:56:00 -0400

    Lesson complete

--- BasicsOfCSharp/Program.cs
+++ BasicsOfCSharp/Program.cs
@@ -15,6 +15,7 @@ namespace BasicsOfCSharp
 
         private static bool MainMenu()
         {
+            Console.Clear();
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Option 1");
             Console.WriteLine("2) Option 2");
@@ -42,13 +43,43 @@ namespace BasicsOfCSharp
 
         private static void PrintNumber()
         {
+            Console.Clear();
             Console.WriteLine("PrintNumbers!");
+            Console.WriteLine("Type a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while(counter < result + 1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
             Console.ReadLine();
         }
 
         private static void GuessingGame()
         {
+            Console.Clear();
             Console.WriteLine("This is a guessing game");
+            Random myRandom = new Random();
+
+            int randomNumber = myRandom.Next(1, 11);
+
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses", guesses);
             Console.ReadLine();
         }
     }
